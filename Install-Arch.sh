d40409aee83539cb089118f461f0351554b12db7
@RubenKelevra <cyrond@gmail.com>
2014-07-12 13:36:52 +0200

    Update Install-Arch.sh

--- Install-Arch.sh
+++ Install-Arch.sh
@@ -7,8 +7,50 @@ admins=("ruben" "sascha")
 sshkeys["ruben"]=""
 sshkeys["sascha"]=""
 
-#run
+archfr_repo="
+[archlinuxfr]
+SigLevel = Optional TrustAll
+Server = http://repo.archlinux.fr/$arch"
+locale_conf='LANG=de_DE.UTF-8
+LC_CTYPE="de_DE.UTF-8"
+LC_NUMERIC="de_DE.UTF-8"
+LC_TIME="de_DE.UTF-8"
+LC_COLLATE="de_DE.UTF-8"
+LC_MONETARY="de_DE.UTF-8"
+LC_MESSAGES="de_DE.UTF-8"
+LC_PAPER="de_DE.UTF-8"
+LC_NAME="de_DE.UTF-8"
+LC_ADDRESS="de_DE.UTF-8"
+LC_TELEPHONE="de_DE.UTF-8"
+LC_MEASUREMENT="de_DE.UTF-8"'
+
 echo "Init complete."
+
+#check preconditions
+echo "Checking internet-connection..."
+
+ping -q 8.8.8.8 -c 4 -i 1 -W 5 >/dev/null 2>&1
+if test $? -ne 0; then
+    echo "ping failed.";exit 1
+fi
+
+ping -q google.com -c 4 -i 1 -W 5 >/dev/null 2>&1
+if test $? -ne 0; then
+    echo "DNS-resolution failed.";exit 1
+fi
+
+maindevice=""
+no_dev=0
+[ -b /dev/sda ] && maindevice="/dev/sda"&&no_dev+=1
+[ -b /dev/vda ] && maindevice="/dev/vda"&&no_dev+=1
+[ -b /dev/hda ] && maindevice="/dev/hda"&&no_dev+=1
+
+if [ $no_dev -gt 1 ]; then
+	echo "more than one maindevice is not supported.";exit 1
+fi
+unset no_dev
+
+#run
 
 REPLY=""
 while [ -z "$REPLY" ]; do
@@ -18,6 +60,33 @@ done
 hostname="$REPLY"
 unset REPLY
 
-maindevice=""
-[ -b /dev/sda ] && maindevice="/dev/sda"
-[ -b /dev/vda ] && maindevice="/dev/vda"
+echo "creating partion..."
+parted -s -- $maindevice mklabel msdos
+parted -a optimal -s -- $maindevice primary btrfs 1M 100%FREE
+
+mainpartition=$(echo "$maindevice")1
+
+echo "creating and mounting new filesystem..."
+mkfs.btrfs -L root $mainpartition
+mount $mainpartition /mnt -O rw,noatime,recovery,compress=zlib,autodefrag,discard,space_cache,inode_cache,nossd
+echo "install basic system..."
+pacstrap /mnt base base-devel grub
+echo "generating fstab entrys..."
+genfstab -Up /mnt >> /mnt/etc/fstab
+
+echo "doing chroot, to configure new system..."
+arch-chroot /mnt /bin/sh <<EOC
+echo $HOSTNAME > /etc/hostname
+ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+echo "$archfr_repo" >> /etc/pacman.conf
+pacman -Syy
+pacman -S yaourt --noconfirm
+yaourt -S rk-server-basic --noconfirm
+sed -i -e 's/#\(de_DE\).UTF-8 UTF-8/\1.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/#\(de_DE\) ISO-8859-1/\1 ISO-8859-1/' /etc/locale.gen
+sed -i -e 's/#\(de_DE\)@euro ISO-8859-15/\1@euro ISO-8859-15/' /etc/locale.gen
+locale-gen&
+cat $locale_conf > /etc/locale.conf
+echo 'KEYMAP="de-latin1"' > /etc/vconsole.conf
+passwd -l root
+
