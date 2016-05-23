54cc5cd43e249344af3a2ce39f71e950f0e3d07d
@RubenKelevra <ruben@vfn-nrw.de>
2016-05-23 18:09:46 +0200

    Update Install-Arch-Server.sh

--- Install-Arch-Server.sh
+++ Install-Arch-Server.sh
@@ -154,10 +154,9 @@ echo "sed -i -e 's/#\(de_DE\)@euro ISO-8859-15/\1@euro ISO-8859-15/' /etc/locale
 echo "locale-gen" >> /mnt/install.sh
 echo "echo '$locale_conf' > /etc/locale.conf" >> /mnt/install.sh
 echo "echo 'KEYMAP=\"de-latin1\"' > /etc/vconsole.conf" >> /mnt/install.sh
-echo "pacman-key --refresh-keys" >> /mnt/install.sh
 echo "echo '$extrarepos' >> /etc/pacman.conf" >> /mnt/install.sh
 echo "dirmngr < /dev/null" >> /mnt/install.sh
-echo "pacman-key -r 5E1ABF240EE7A126 && pacman-key --refresh-keys && pacman-key --lsign-key 5E1ABF240EE7A126" >> /mnt/install.sh
+echo "pacman-key -r 5E1ABF240EE7A126 && pacman-key --lsign-key 5E1ABF240EE7A126" >> /mnt/install.sh
 echo "pacman -Syy" >> /mnt/install.sh
 echo "pacman -S yaourt --noconfirm" >> /mnt/install.sh
 echo "sed -i -e 's/ -mtune=generic / -mtune=native /g' /etc/makepkg.conf" >> /mnt/install.sh
