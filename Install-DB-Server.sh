1ab53632374a6afa13db9267dcc85a7abd5e7968
@RubenKelevra <ruben@vfn-nrw.de>
2016-06-07 01:23:59 +0200

    Update Install-DB-Server.sh

--- Install-DB-Server.sh
+++ Install-DB-Server.sh
@@ -83,7 +83,7 @@ echo "creating partion..."
 parted -s -- $maindevice mklabel msdos
 parted -a optimal -s -- $maindevice mkpart primary ext4 4096S 120MB
 parted -a optimal -s -- $maindevice mkpart primary linux-swap 120M 1500MB
-parted -a optimal -s -- $maindevice mkpart primary ext4 1500MB 99%FREE
+parted -a optimal -s -- $maindevice mkpart primary ext4 1500MB 92%FREE
 
 bootpartition=$(echo "$maindevice")1
 swappartition=$(echo "$maindevice")2
